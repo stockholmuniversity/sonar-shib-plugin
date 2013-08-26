4a803970a27d8257307269f726008f5fcadc7837
Joakim Lundin <joakim.lundin@su.se>
2013-08-26 07:50:55 +0200

    Indent

--- src/test/groovy/se/su/it/sonar/plugin/shibboleth/ShibAuthenticatiorSpec.groovy
+++ src/test/groovy/se/su/it/sonar/plugin/shibboleth/ShibAuthenticatiorSpec.groovy
@@ -9,18 +9,17 @@ import javax.servlet.http.HttpServletRequest
 class ShibAuthenticatiorSpec extends Specification {
 
   def "Test that doAuthenticate gets the correct attribute from the request"() {
-  given:
-  def request = Mock(HttpServletRequest)
-  def userDetails = new UserDetails()
-  def context = new Authenticator.Context("", "", request)
+    given:
+    def request = Mock(HttpServletRequest)
+    def userDetails = new UserDetails()
+    def context = new Authenticator.Context("", "", request)
 
-  request.getAttribute(ShibValidationFilter.USER_ATTRIBUTE) >> userDetails
+    request.getAttribute(ShibValidationFilter.USER_ATTRIBUTE) >> userDetails
+  
+    when:
+    def ret = new ShibAuthenticator().doAuthenticate(context)
 
-  when:
-  def ret = new ShibAuthenticator().doAuthenticate(context)
-
-  then:
-  assert ret
-}
-
+    then:
+    assert ret
+  }
 }
